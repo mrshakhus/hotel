ae8e857e6363c02fee7429f641b0bbdaf9886ec6
Shakhboz Usmanov <shakhbozusmanov@gmail.com>
2024-09-02 18:29:02 +0300

    deleted com

--- init-db.sh
+++ init-db.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Создание пользователя и базы данных, используя нестандартные переменные окружения
-# if [ -n "$TEST_POSTGRES_USER" ] && [ -n "$TEST_POSTGRES_PASSWORD" ]; then
-#   psql -U postgres -c "CREATE USER $TEST_POSTGRES_USER WITH PASSWORD '$TEST_POSTGRES_PASSWORD';"
-# fi
-
 if [ -n "$TEST_POSTGRES_DB" ]; then
   psql -U postgres -c "CREATE DATABASE $TEST_POSTGRES_DB;"
 fi
